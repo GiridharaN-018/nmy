8aac1c04cc41011ec19a9e46cb1102a5248b1dba
GiridharaN-018 <giridharanakp018@gmail.com>
2025-07-17 14:28:44 +0530

    Update async.js

--- async.js
+++ async.js
@@ -1,7 +1,13 @@
-let myfunction= async()=>{
+/*let myfunction= async()=>{
   return "hello";
 }
 myfunction().then((msg)=>{
   console.log(msg)
+}*/
+let data=async()=>{
+  let msg= await 'this is the example of ansync and await';
+  console.log(msg)
 }
-                  
+  console.log("before function call ")
+data()
+console.log("AFter function call")
