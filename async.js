18eadea6c3dfb4923e813b7b002007354a885a0b
GiridharaN-018 <giridharanakp018@gmail.com>
2025-07-17 14:36:25 +0530

    Update async.js

--- async.js
+++ async.js
@@ -3,11 +3,31 @@
 }
 myfunction().then((msg)=>{
   console.log(msg)
-}*/
+}
 let data=async()=>{
   let msg= await 'this is the example of ansync and await';
   console.log(msg)
 }
   console.log("before function call ")
 data()
-console.log("AFter function call")
+console.log("AFter function call")*/
+
+int show =async()={
+  let promise =new Promise((res,rej)=>{
+    var marks=5;
+    if(marks<=6){
+    {
+      res("accepted")
+    }
+    else{
+      rej("Rejected")
+    }
+  }
+  try{
+    var result=await promise;
+    console.log(result)
+  }catch(error){
+    console.log(error)
+  }
+} 
+show()
